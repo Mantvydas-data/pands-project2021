2ade15bf2c5f8bb9ceddb5cd871a095c03f90fd7
Mantvydas-data <G00398349@gmit.ie>
2021-04-25 18:29:36 +0100

    Writting to txt file option

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Summary: Iris flower data set is a multivariable data set collected by Sir Ronal
 Dataset contains 150 observations altogether, 50 observations for each species. Features include length and width of sepal and petal, also Iris species name.
 
 <p align="center"><img src="https://www.fs.fed.us/wildflowers/beauty/iris/images/flower/blueflagiris_flower_lg.jpg" alt="flower/blueflagiris_flower_lg" border="0" width=300px></p><br>
-<p align="left">Iris flower sepal and petal. From U.S Forest Service website: https://www.fs.fed.us/wildflowers/beauty/iris/flower.shtml</p>
+<p align="left">Iris flower sepal and petal from U.S Forest Service website: https://www.fs.fed.us/wildflowers/beauty/iris/flower.shtml</p>
 
 
 Analysis of this dataset will be performed using Python, that is a powerful high level programming language. Various Python packages will be used to explore and summarise the findings. 
@@ -22,5 +22,9 @@ https://www.w3schools.com/html/html_paragraphs.asp
 https://archive.ics.uci.edu/ml/datasets/Iris
 https://gist.github.com/curran/a08a1080b88344b0c8a7
 https://cmdlinetips.com/2018/02/how-to-subset-pandas-dataframe-based-on-values-of-a-column/
-
+https://www.guru99.com/reading-and-writing-files-in-python.html
+https://www.w3schools.com/python/python_ref_functions.asp
+https://ourcodingclub.github.io/tutorials/pandas-python-intro/
+https://www.w3schools.com/python/python_strings_escape.asp
+https://thispointer.com/how-to-append-text-or-lines-to-a-file-in-python/
 
--- analysis.py
+++ analysis.py
@@ -4,6 +4,8 @@
 # Importing packages to be used
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 irissrc = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 
@@ -11,8 +13,10 @@ irissrc = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.d
 iris = pd.read_csv(irissrc, sep=',', names=["sepallength", "sepalwidth", "petallength", "petalwidth", "species"])
 iris.to_csv('iris.csv')
 
-# Creating 15 instances data sample and confirming value counts by species, checking for NAs
+# Checking data shape, missing data, sample size of 15, value counts by species
+shape = np.shape(iris)
 na = iris.isnull().sum()
+head = iris.head(10) 
 sample = iris.sample(15)
 counts = iris["species"].value_counts()
 
@@ -25,4 +29,24 @@ versicolor = iris.loc[iris["species"] == "Iris-versicolor"]
 descr1 = pd.DataFrame(setosa).describe()
 descr2 = pd.DataFrame(virginica).describe()
 descr3 = pd.DataFrame(versicolor).describe()
-print(descr1)
+
+# Output to summary text file:
+with open("summary.txt", "a+") as f:
+    f.write("\n\t This file provides summary of Iris dataset analysis \n\n")
+    f.write("Dataset shape is (lines, columns): \n{}\n\n".format(shape))
+    f.write("Check for any missing values: \n{}\n\n".format(na))
+    f.write("Top of dataset: \n{}\n\n".format(head))
+    f.write("Data sample of 15 instances: \n{} \n\n".format(sample))
+    f.write("\n\n\t Decriptive statistics by Iris flower species: \n\n")
+    f.write("Setosa:\n{}\n\n".format(descr1))
+    f.write("Virginica:\n{}\n\n".format(descr2))
+    f.write("Versicolor:\n{}\n\n".format(descr3))
+
+# Plots 
+#sns
+
+# Saving plots
+
+#plt.show()
+#plt.savefig("plot_name.png")
+print("ALL works ook")
